7353b756d56bd549a06fa43b655c7ca75078f761
out4b <seeds.c@gmail.com>
2015-11-28 11:48:59 +0800

    indent update for test case

--- test/test1.js
+++ test/test1.js
@@ -10,13 +10,13 @@ describe('discover all devices', function() {
   this.timeout(3000);
   it('discover OK', function(done) {
     request(url).post('/discover').expect(200).end(function(err, res) {
-        if(err) throw err;
-        setTimeout(function() {
-          request(url).post('/stop-discover').expect(200).end(function(err, res) {
-              if(err) throw err;
-              done();
-          });
-        }, 2000);
+      if(err) throw err;
+      setTimeout(function() {
+        request(url).post('/stop-discover').expect(200).end(function(err, res) {
+          if(err) throw err;
+          done();
+        });
+      }, 2000);
     });
   });
 });
@@ -26,28 +26,28 @@ describe('get device list', function() {
     request(url).get('/device-list')
     .expect('Content-Type', /json/)
     .expect(200).end(function(err, res) {
-        if(err) throw err;
-        for (var i in res.body) {
-          res.body[i].should.have.property('configId').which.is.a.Number();
-          res.body[i].should.have.property('specVersion').and.have.property('major', 1);
-          res.body[i].should.have.property('specVersion').and.have.property('minor', 0);
-          res.body[i].should.have.property('device');
-          var device = res.body[i].device;
-          device.should.have.property('deviceType');
-          device.should.have.property('friendlyName');
-          device.should.have.property('manufacturer');
-          // device.should.have.property('modelName');
-          device.should.have.property('userAuth');
-          device.should.have.property('serviceList', {});
-          // if (device.deviceType != 'urn:cdif-net:device:BinaryLight:1' &&
-          //   device.deviceType != 'urn:cdif-net:device:DimmableLight:1' &&
-          //   device.deviceType != 'urn:cdif-net:device:SensorHub:1' &&
-          //   device.deviceType != 'urn:cdif-net:device:ONVIFCamera:1') {
-          //     throw(new Error('unknown device type: ' + device.deviceType));
-          //   }
-        }
-        deviceList = JSON.parse(JSON.stringify(res.body));
-        done();
+      if(err) throw err;
+      for (var i in res.body) {
+        res.body[i].should.have.property('configId').which.is.a.Number();
+        res.body[i].should.have.property('specVersion').and.have.property('major', 1);
+        res.body[i].should.have.property('specVersion').and.have.property('minor', 0);
+        res.body[i].should.have.property('device');
+        var device = res.body[i].device;
+        device.should.have.property('deviceType');
+        device.should.have.property('friendlyName');
+        device.should.have.property('manufacturer');
+        // device.should.have.property('modelName');
+        device.should.have.property('userAuth');
+        device.should.have.property('serviceList', {});
+        // if (device.deviceType != 'urn:cdif-net:device:BinaryLight:1' &&
+        //   device.deviceType != 'urn:cdif-net:device:DimmableLight:1' &&
+        //   device.deviceType != 'urn:cdif-net:device:SensorHub:1' &&
+        //   device.deviceType != 'urn:cdif-net:device:ONVIFCamera:1') {
+        //     throw(new Error('unknown device type: ' + device.deviceType));
+        //   }
+      }
+      deviceList = JSON.parse(JSON.stringify(res.body));
+      done();
     });
   });
 });
@@ -116,9 +116,9 @@ function testInvokeActions(deviceID, serviceID, serviceList, callback) {
       action.should.be.not.empty;
       var args = action.argumentList;
       var req = { serviceID: serviceID,
-                  actionName: name,
-                  argumentList: {},
-                  access_token: deviceList[deviceID].access_token
+        actionName: name,
+        argumentList: {},
+        access_token: deviceList[deviceID].access_token
       };
       for (var j in args) {
         var argName = j;
@@ -131,46 +131,46 @@ function testInvokeActions(deviceID, serviceID, serviceList, callback) {
         stateVar.should.be.an.Object;
         stateVar.should.be.not.empty;
         if (stateVar.dataType === 'number' ||
-            stateVar.dataType === 'uint8' ||
-            stateVar.dataType === 'uint16' ||
-            stateVar.dataType === 'uint32' ||
-            stateVar.dataType === 'sint8' ||
-            stateVar.dataType === 'sint16' ||
-            stateVar.dataType === 'sint32'
-        ) {
-          var min = 0; var max = 100;
-          if (stateVar.allowedValueRange) {
-            stateVar.allowedValueRange.minimum.should.be.a.Number;
-            stateVar.allowedValueRange.maximum.should.be.a.Number;
-            min = stateVar.allowedValueRange.minimum;
-            max = stateVar.allowedValueRange.maximum;
-          }
-          req.argumentList[argName] = Math.floor(Math.random() * max) + min;
-        } else if (stateVar.dataType === 'boolean') {
-          req.argumentList[argName] = Math.random() >= 0.5;
-        } else if (stateVar.dataType === 'string') {
-          if (stateVar.defaultValue) {
-            req.argumentList[argName] = stateVar.defaultValue;
-          } else {
-            req.argumentList[argName] = 'test';
-          }
-        } else if (stateVar.dataType === 'object') {
-          req.argumentList[argName] = {};   // fix this after we have object type schema
-        } else if (stateVar.dataType === 'url') {
-          req.argumentList[argName] = 'http://test.com';
+        stateVar.dataType === 'uint8' ||
+        stateVar.dataType === 'uint16' ||
+        stateVar.dataType === 'uint32' ||
+        stateVar.dataType === 'sint8' ||
+        stateVar.dataType === 'sint16' ||
+        stateVar.dataType === 'sint32'
+      ) {
+        var min = 0; var max = 100;
+        if (stateVar.allowedValueRange) {
+          stateVar.allowedValueRange.minimum.should.be.a.Number;
+          stateVar.allowedValueRange.maximum.should.be.a.Number;
+          min = stateVar.allowedValueRange.minimum;
+          max = stateVar.allowedValueRange.maximum;
         }
+        req.argumentList[argName] = Math.floor(Math.random() * max) + min;
+      } else if (stateVar.dataType === 'boolean') {
+        req.argumentList[argName] = Math.random() >= 0.5;
+      } else if (stateVar.dataType === 'string') {
+        if (stateVar.defaultValue) {
+          req.argumentList[argName] = stateVar.defaultValue;
+        } else {
+          req.argumentList[argName] = 'test';
+        }
+      } else if (stateVar.dataType === 'object') {
+        req.argumentList[argName] = {};   // fix this after we have object type schema
+      } else if (stateVar.dataType === 'url') {
+        req.argumentList[argName] = 'http://test.com';
       }
-      console.log('Request:' + JSON.stringify(req));
-      request(url).post('/device-control/' + deviceID + '/invoke-action')
-      .send(req)
-      .expect('Content-Type', /[json | text]/)
-      .expect(200, function(err, res) {
-        if (err) {
-          console.error(err);
-        }
-        console.log('Response: ' + JSON.stringify(res.body));
-        cb();
-      });
-    }, 5000);
-  }, callback);
+    }
+    console.log('Request:' + JSON.stringify(req));
+    request(url).post('/device-control/' + deviceID + '/invoke-action')
+    .send(req)
+    .expect('Content-Type', /[json | text]/)
+    .expect(200, function(err, res) {
+      if (err) {
+        console.error(err);
+      }
+      console.log('Response: ' + JSON.stringify(res.body));
+      cb();
+    });
+  }, 5000);
+}, callback);
 }
