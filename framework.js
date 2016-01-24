aed779e0ab3300578c779afec06419f5830425e7
out4b <stavrogin6@gmail.com>
2016-01-24 14:58:28 +0800

    further cleanup, remove dm dep from rm

--- framework.js
+++ framework.js
@@ -1,10 +1,8 @@
 var ModuleManager = require('./module-manager');
-var DeviceManager = require('./lib/device-manager');
 var RouteManager = require('./lib/route-manager');
 
 var mm = new ModuleManager();
-var deviceManager = new DeviceManager(mm);
-var routeManager = new RouteManager(deviceManager);
+var routeManager = new RouteManager(mm);
 
 
 routeManager.installRoutes();
