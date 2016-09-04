43afa1d346e7d83f439be2ebf8e7753ddb6f423d
out4b <seeds.c@gmail.com>
2016-09-04 11:38:53 +0800

    rename module-manager move schema generation code to device class enable getting schema from REST interface improve test code to suport fake complex data based on its schema

--- test/test1.js
+++ test/test1.js
@@ -1,6 +1,7 @@
-var should = require('should');
+var should  = require('should');
 var request = require('supertest');
-var async = require('async');
+var async   = require('async');
+var faker   = require('json-schema-faker');
 
 var url = 'http://localhost:3049';
 
@@ -115,27 +116,29 @@ function testInvokeActions(deviceID, serviceID, serviceList, callback) {
       action.should.be.an.Object;
       action.should.be.not.empty;
       var args = action.argumentList;
+
+      var argList = Object.keys(action.argumentList);
       var req = { serviceID: serviceID,
         actionName: name,
         argumentList: {},
         device_token: deviceList[deviceID].device_token
       };
-      for (var j in args) {
-        var argName = j;
-        argName.should.not.be.empty;
-        var stateVarName = args[j].relatedStateVariable;
+      async.eachSeries(argList, function(arg, call_back) {
+        arg.should.not.be.empty;
+        var stateVarName = action.argumentList[arg].relatedStateVariable;
         var stateVarTable = serviceList[serviceID].serviceStateTable;
         stateVarTable.should.be.an.Object;
         stateVarTable.should.be.not.empty;
         var stateVar = stateVarTable[stateVarName];
         stateVar.should.be.an.Object;
         stateVar.should.be.not.empty;
-        if (stateVar.dataType === 'number' ||
-            stateVar.dataType === 'uint8' ||
-            stateVar.dataType === 'uint16' ||
-            stateVar.dataType === 'uint32' ||
-            stateVar.dataType === 'sint8' ||
-            stateVar.dataType === 'sint16' ||
+        if (stateVar.dataType === 'number'  ||
+            stateVar.dataType === 'integer' ||
+            stateVar.dataType === 'uint8'   ||
+            stateVar.dataType === 'uint16'  ||
+            stateVar.dataType === 'uint32'  ||
+            stateVar.dataType === 'sint8'   ||
+            stateVar.dataType === 'sint16'  ||
             stateVar.dataType === 'sint32') {
           var min = 0; var max = 100;
           if (stateVar.allowedValueRange) {
@@ -145,34 +148,49 @@ function testInvokeActions(deviceID, serviceID, serviceList, callback) {
             max = stateVar.allowedValueRange.maximum;
           }
           if (stateVar.defaultValue) {
-            req.argumentList[argName] = stateVar.defaultValue;
+            req.argumentList[arg] = stateVar.defaultValue;
           } else {
-            req.argumentList[argName] = Math.floor(Math.random() * max) + min;
+            req.argumentList[arg] = Math.floor(Math.random() * max) + min;
           }
+          call_back();
         } else if (stateVar.dataType === 'boolean') {
-          req.argumentList[argName] = Math.random() >= 0.5;
+          req.argumentList[arg] = Math.random() >= 0.5;
+          call_back();
         } else if (stateVar.dataType === 'string') {
           if (stateVar.defaultValue) {
-            req.argumentList[argName] = stateVar.defaultValue;
+            req.argumentList[arg] = stateVar.defaultValue;
           } else {
-            req.argumentList[argName] = 'test';
+            req.argumentList[arg] = 'test';
           }
+          call_back();
         } else if (stateVar.dataType === 'object') {
-          req.argumentList[argName] = {};   // fix this after we have object type schema
-        } else if (stateVar.dataType === 'url') {
-          req.argumentList[argName] = 'http://test.com';
+          var schemaRef = stateVar.schema;
+          schemaRef.should.be.a.String;
+          request(url).get('/device-control/' + deviceID + '/schema' + schemaRef)
+          .send({"device_token": deviceList[deviceID].device_token})
+          .expect(200, function(err, res) {
+            if (err) throw err;
+            var variableSchema = res.body;
+            variableSchema.should.be.an.Object;
+            variableSchema.should.be.not.empty;
+            var fake_data = faker(variableSchema);
+            console.log(fake_data);
+            req.argumentList[arg] = fake_data;
+            call_back();
+          });
         }
-      }
-      console.log('Request:' + JSON.stringify(req));
-      request(url).post('/device-control/' + deviceID + '/invoke-action')
-      .send(req)
-      .expect('Content-Type', /[json | text]/)
-      .expect(200, function(err, res) {
-        if (err) {
-          console.error(err);
-        }
-        console.log('Response: ' + JSON.stringify(res.body));
-        cb();
+      }, function() {
+        console.log('Request:' + JSON.stringify(req));
+        request(url).post('/device-control/' + deviceID + '/invoke-action')
+        .send(req)
+        .expect('Content-Type', /[json | text]/)
+        .expect(200, function(err, res) {
+          if (err) {
+            console.error(err);
+          }
+          console.log('Response: ' + JSON.stringify(res.body));
+          cb();
+        });
       });
     }, 5000);
   }, callback);
