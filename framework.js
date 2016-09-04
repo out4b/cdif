010230207431c164ff685bd392555d21e8d4a7d6
out4b <seeds.c@gmail.com>
2016-09-04 11:38:53 +0800

    work with cdif-ble-manager npm module code styling change

--- framework.js
+++ framework.js
@@ -37,57 +37,57 @@ var actionInvokeRouter = express.Router({mergeParams: true});
 var getDeviceSpecRouter = express.Router({mergeParams: true});
 
 discoverRouter.route('/').post(function(req, res) {
-    mm.discoverAllDevices();
-    res.sendStatus(200);
+  mm.discoverAllDevices();
+  res.sendStatus(200);
 });
 
 stopDiscoverRouter.route('/').post(function(req, res) {
-    mm.stopDiscoverAllDevices();
-    res.sendStatus(200);
+  mm.stopDiscoverAllDevices();
+  res.sendStatus(200);
 });
 
 
 deviceListRouter.route('/').get(function(req, res) {
-    var deviceList = [];
-    for (var id in deviceMap) {
-        deviceList.push(id);
-    }
-    res.status(200).send(JSON.stringify(deviceList));
+  var deviceList = [];
+  for (var id in deviceMap) {
+    deviceList.push(id);
+  }
+  res.status(200).send(JSON.stringify(deviceList));
 });
 
 connectRouter.route('/').post(function(req, res) {
-    var deviceID = req.params.deviceID;
-    var device = deviceMap[deviceID];
-    if (!device) {
-        res.status(500).send(JSON.stringify('device not found'));
-        return;
+  var deviceID = req.params.deviceID;
+  var device = deviceMap[deviceID];
+  if (!device) {
+    res.status(500).send(JSON.stringify('device not found'));
+    return;
+  }
+  var deviceObj = device.obj;
+  //TODO: add auth support
+  deviceObj.connect('', '', function(err) {
+    if (err) {
+      res.status(500).send(JSON.stringify(err));
+    } else {
+      res.sendStatus(200);
     }
-    var deviceObj = device.obj;
-    //TODO: add auth support
-    deviceObj.connect('', '', function(err) {
-        if (err) {
-            res.status(500).send(JSON.stringify(err));
-        } else {
-            res.sendStatus(200);
-        }
-    });
+  });
 });
 
 disconnectRouter.route('/').post(function(req, res) {
-    var deviceID = req.params.deviceID;
-    var device = deviceMap[deviceID];
-    if (!device) {
-        res.status(500).send(JSON.stringify('device not found'));
-        return;
+  var deviceID = req.params.deviceID;
+  var device = deviceMap[deviceID];
+  if (!device) {
+    res.status(500).send(JSON.stringify('device not found'));
+    return;
+  }
+  var deviceObj = device.obj;
+  deviceObj.disconnect(function(err) {
+    if (err) {
+      res.status(500).send(JSON.stringify(err));
+    } else {
+      res.sendStatus(200);
     }
-    var deviceObj = device.obj;
-    deviceObj.disconnect(function(err) {
-        if (err) {
-            res.status(500).send(JSON.stringify(err));
-        } else {
-            res.sendStatus(200);
-        }
-    });
+  });
 });
 
 
@@ -97,45 +97,45 @@ disconnectRouter.route('/').post(function(req, res) {
 //TODO: check output schema (output object should contain elements with their valid argument names and what returned to client should have retval property)
 
 actionInvokeRouter.route('/').post(function(req, res) {
-    var deviceID = req.params.deviceID;
-    //TODO: validate input command schema
-    //TODO: input argumentList should contain elements with their valid argument names)
-    var serviceId = req.body.serviceId;
-    var actionName = req.body.actionName;
-    var args = req.body.argumentList;
-    var device = deviceMap[deviceID];
-    if (!device) {
-        res.status(500).send(JSON.stringify('device not found'));
-        return;
-    }
-    var deviceObj = device.obj;
-    if (!deviceObj) {
-        res.status(500).send(JSON.stringify('device not found'));
-    } else {
-        try {
-            deviceObj.deviceControl(serviceId, actionName, args, function(err, ret) {
-                if(err) {
-                    res.status(500).send(JSON.stringify(err));
-                } else {
-                    //TODO: update device.states in device model
-                    res.status(200).send(JSON.stringify(ret));
-                }
-            });
-        } catch (e) {
-            res.status(500).send(JSON.stringify(e.message));
+  var deviceID = req.params.deviceID;
+  //TODO: validate input command schema
+  //TODO: input argumentList should contain elements with their valid argument names)
+  var serviceId = req.body.serviceId;
+  var actionName = req.body.actionName;
+  var args = req.body.argumentList;
+  var device = deviceMap[deviceID];
+  if (!device) {
+    res.status(500).send(JSON.stringify('device not found'));
+    return;
+  }
+  var deviceObj = device.obj;
+  if (!deviceObj) {
+    res.status(500).send(JSON.stringify('device not found'));
+  } else {
+    try {
+      deviceObj.deviceControl(serviceId, actionName, args, function(err, ret) {
+        if(err) {
+          res.status(500).send(JSON.stringify(err));
+        } else {
+          //TODO: update device.states in device model
+          res.status(200).send(JSON.stringify(ret));
         }
+      });
+    } catch (e) {
+      res.status(500).send(JSON.stringify(e.message));
     }
+  }
 });
 
 getDeviceSpecRouter.route('/').get(function(req, res) {
-    var deviceID = req.params.deviceID;
-    var device = deviceMap[deviceID];
-    if (!device) {
-        res.status(500).send(JSON.stringify('device not found'));
-        return;
-    }
-    var spec = deviceMap[deviceID].spec;
-    res.status(200).send(spec);
+  var deviceID = req.params.deviceID;
+  var device = deviceMap[deviceID];
+  if (!device) {
+    res.status(500).send(JSON.stringify('device not found'));
+    return;
+  }
+  var spec = deviceMap[deviceID].spec;
+  res.status(200).send(spec);
 });
 
 app.use(morgan('dev'));
@@ -169,78 +169,77 @@ db.serialize(function() {
 });
 
 mm.on('deviceonline', function(discovered, module) {
-    var spec = null;
-    discovered.getDeviceSpec(function(err, data){
-        if (!err) {
-            console.log(data);
-            spec = JSON.parse(data);
-        }
-    });
-    //TODO: validate device spec schema
-    if (spec == null) return;
-
-    var device = {};
-    device.spec = spec;
-    device.module = module;
-    device.obj = discovered;
-
-    device.states = {};
-    var services = spec.device.serviceList;
-    for (var i in services) {
-        var service = services[i];
-        device.states[i] = {};
-        var stateVariables = service.serviceStateTable;
-        for (var j in stateVariables) {
-            if (stateVariables[j].hasOwnProperty('defaultValue')) {
-                device.states[i][j] = stateVariables[j].defaultValue;
-            } else {
-                device.states[i][j] = '';
-            }
-        }
+  var spec = null;
+  discovered.getDeviceSpec(function(err, data){
+    if (!err) {
+      console.log(data);
+      spec = JSON.parse(data);
     }
+  });
+  //TODO: validate device spec schema
+  if (spec == null) return;
 
-    var hwAddr;
-    if (!spec.device.UDN) {
-        discovered.getHWAddress(function(err, data) {
-            if(!err) {
-                hwAddr = data;
-            } else {
-                console.error('cannot get HW address for device' + discovered);
-            }
-        });
-    } else {
-        hwAddr = spec.device.UDN;
-    }
-    db.get("SELECT uuid FROM device_addr WHERE hwaddr = ?", hwAddr, function (err, data) {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      var deviceUUID;
-      if (!data) {
-          deviceUUID = uuid.v4();
-          db.run("INSERT INTO device_addr(hwaddr, uuid) VALUES (?, ?)", hwAddr, deviceUUID);
+  var device = {};
+  device.spec = spec;
+  device.module = module;
+  device.obj = discovered;
+
+  device.states = {};
+  var services = spec.device.serviceList;
+  for (var i in services) {
+    var service = services[i];
+    device.states[i] = {};
+    var stateVariables = service.serviceStateTable;
+    for (var j in stateVariables) {
+      if (stateVariables[j].hasOwnProperty('defaultValue')) {
+        device.states[i][j] = stateVariables[j].defaultValue;
       } else {
-          deviceUUID = data.uuid;
+        device.states[i][j] = '';
       }
-      deviceMap[deviceUUID] = device;
-      console.log("device name: " + spec.device.friendlyName + '\t' + "UUID: " + deviceUUID);
+    }
+  }
+
+  var hwAddr;
+  if (!spec.device.UDN) {
+    discovered.getHWAddress(function(err, data) {
+      if(!err) {
+        hwAddr = data;
+      } else {
+        console.error('cannot get HW address for device' + discovered);
+      }
     });
+  } else {
+    hwAddr = spec.device.UDN;
+  }
+  db.get("SELECT uuid FROM device_addr WHERE hwaddr = ?", hwAddr, function (err, data) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    var deviceUUID;
+    if (!data) {
+      deviceUUID = uuid.v4();
+      db.run("INSERT INTO device_addr(hwaddr, uuid) VALUES (?, ?)", hwAddr, deviceUUID);
+    } else {
+      deviceUUID = data.uuid;
+    }
+    deviceMap[deviceUUID] = device;
+    console.log("device name: " + spec.device.friendlyName + '\t' + "UUID: " + deviceUUID);
+  });
 });
 
 mm.on('deviceoffline', function(device) {
 });
 
 function init() {
-    try {
-      mm.loadModules();
-    } catch (e) {
-      console.error(e);
-      process.exit(-1);
-    }
+  try {
+    mm.loadModules();
+  } catch (e) {
+    console.error(e);
+    process.exit(-1);
+  }
 }
 
 init();
 
 app.listen(appPort);
-
