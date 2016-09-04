ab666a4e3be2b4b742f062fb82a8f7ccd53a5233
out4b <seeds.c@gmail.com>
2016-09-04 11:38:53 +0800

    test code update

--- test/test1.js
+++ test/test1.js
@@ -51,53 +51,90 @@ describe('get device list', function() {
 
 describe('connect all devices', function() {
   this.timeout(5000);
-  it('connect device OK', function(done) {
+  var deviceList;
+  before(function(done) {
     request(url).get('/device-list')
     .expect('Content-Type', /json/)
     .expect(200).end(function(err, res) {
-      var tasks = [];
-      var callback = function(err) {
-        if (err) throw err;
-      }
-      for (var i in res.body) {
-        var connect = function(callback) {
-          request(url).post('/device-control/' + i + '/connect')
-          .expect(200, callback);
-        }
-        tasks.push(connect);
-      }
-      async.series(tasks, done);
+      if (err) throw err;
+      deviceList = res.body;
+      done();
     });
+  });
+
+  it('connect OK', function(done) {
+    var list = [];
+    for (var i in deviceList) {
+      list.push(i);
+    }
+    async.eachSeries(list, function(deviceID, callback) {
+      request(url).post('/device-control/' + deviceID + '/connect')
+      .expect(200, callback);
+    }, done);
   });
 });
 
 describe('invoke device actions', function() {
   this.timeout(15000);
-  it('invoke OK', function(done) {
+  var deviceList;
+  before(function(done) {
     request(url).get('/device-list')
     .expect('Content-Type', /json/)
     .expect(200).end(function(err, res) {
-      var get_spec_tasks = [];
+      if (err) throw err;
+      deviceList = res.body;
+      done();
+    });
+  });
 
-      var getSpecCallback = function(err, response) {
-        console.log(response);
-      }
-      for (var i in res.body) {
-        var getSpec = function(getSpecCallback) {
-          request(url).get('/device-control/' + i + '/get-spec')
-          .set('Accept', 'application/json')
-          .expect('Content-Type', /json/)
-          .expect(200)
-          .end(function(err, result) {
-            console.log(result.body);
-            if (err) throw err;
-            getSpecCallback(err, result.body);
-          });
+  it('invoke OK', function(done) {
+    var list = [];
+    for (var i in deviceList) {
+      list.push(i);
+    }
+    async.eachSeries(list, function(deviceID, callback) {
+      request(url).get('/device-control/' + deviceID + '/get-spec')
+      .expect(200, function(err, res) {
+        if (err) throw err;
+        var device = res.body.device;
+        device.should.have.property('serviceList');
+        device.serviceList.should.be.an.Object;
+        device.serviceList.should.be.not.empty;
+        var serviceList = [];
+        for (var serviceID in device.serviceList) {
+          serviceList.push(serviceID);
         }
-        get_spec_tasks.push(getSpec);
-      }
-      async.series(get_spec_tasks, done);
-
+        async.eachSeries(serviceList, function(serviceID, cb) {
+          testInvokeActions(deviceID, serviceID, res.body.device.serviceList, cb);
+        }, function(){});
+        callback();
+      });
+    }, done);
+  });
+});
+  //
+  //
+  // it('invoke OK', function(done) {
+  //   request(url).get('/device-list')
+  //   .expect('Content-Type', /json/)
+  //   .expect(200).end(function(err, res) {
+  //     var get_spec_tasks = [];
+  //
+  //     for (var deviceID in res.body) {
+  //       var getSpec = function(cb) {
+  //         request(url).get('/device-control/' + deviceID + '/get-spec')
+  //         .set('Accept', 'application/json')
+  //         .expect('Content-Type', /json/)
+  //         .expect(200)
+  //         .end(function(err, result) {
+  //           if (err) throw err;
+  //           cb(err, result.body, deviceID);
+  //         });
+  //       }
+  //       get_spec_tasks.push(getSpec);
+  //     }
+  //     async.series(get_spec_tasks, done);
+  //
 
       // var deviceList = Object.keys(res.body);
       // // for (var i = 0; i < deviceList.length; i++) {
@@ -118,52 +155,55 @@ describe('invoke device actions', function() {
       // //     }
       // //   });
       // // }
+
+
+
+
+function testInvokeActions(deviceID, serviceID, serviceList, callback) {
+  var actionList = serviceList[serviceID].actionList;
+  actionList.should.be.an.Object;
+  actionList.should.be.not.empty;
+  var list = [];
+  for (var i in actionList) {
+    list.push(i);
+  }
+  async.eachSeries(list, function(actionName, cb) {
+    var action = actionList[actionName];
+    action.should.be.an.Object;
+    action.should.be.not.empty;
+    var args = action.argumentList;
+    var req = { serviceID: serviceID,
+                actionName: i,
+                argumentList: {}
+    };
+    for (var j in args) {
+      var argName = j;
+      argName.should.not.be.empty;
+      var stateVarName = args[j].relatedStateVariable;
+      var stateVarTable = serviceList[serviceID].serviceStateTable;
+      stateVarTable.should.be.an.Object;
+      stateVarTable.should.be.not.empty;
+      var stateVar = stateVarTable[stateVarName];
+      stateVar.should.be.an.Object;
+      stateVar.should.be.not.empty;
+      if (stateVar.dataType === 'number') {
+        var min = 0; var max = 100;
+        if (stateVar.allowedValueRange) {
+          min = stateVar.allowedValueRange.minimum;
+          max = stateVar.allowedValueRange.maximum;
+        }
+        req.argumentList[argName] = Math.floor(Math.random() * max) + min;
+      } else if (stateVar.dataType === 'boolean') {
+        req.argumentList[argName] = Math.random() >= 0.5;
+      }
+    }
+    request(url).post('/device-control/' + deviceID + '/invoke-action')
+    .send(req)
+    .expect('Content-Type', /json/)
+    .expect(200).end(function(err, res) {
+      console.log(res.body);
+      cb();
     });
-  });
-});
-
-
-
-// function testInvokeActions(deviceID, service, serviceID) {
-//   var actionList = service.actionList;
-//   actionList.should.be.an.Object;
-//   actionList.should.be.not.empty;
-//   for (var i in actionList) {
-//     var action = actionList[i];
-//     action.should.be.an.Object;
-//     action.should.be.not.empty;
-//     var args = action.argumentList;
-//     var req = { serviceID: serviceID,
-//                 actionName: i,
-//                 argumentList: {}
-//     };
-//     for (var j in args) {
-//       var argName = j;
-//       argName.should.not.be.empty;
-//       var stateVarName = args[j].relatedStateVariable;
-//       var stateVarTable = service.serviceStateTable;
-//       stateVarTable.should.be.an.Object;
-//       stateVarTable.should.be.not.empty;
-//       var stateVar = stateVarTable[stateVarName];
-//       stateVar.should.be.an.Object;
-//       stateVar.should.be.not.empty;
-//       if (stateVar.dataType === 'number') {
-//         var min = 0; var max = 100;
-//         if (stateVar.allowedValueRange) {
-//           min = stateVar.allowedValueRange.minimum;
-//           max = stateVar.allowedValueRange.maximum;
-//         }
-//         req.argumentList[argName] = Math.floor(Math.random() * max) + min;
-//       } else if (stateVar.dataType === 'boolean') {
-//         req.argumentList[argName] = Math.random() >= 0.5;
-//       }
-//     }
-//     request(url).post('/device-control/' + deviceID + '/invoke-action')
-//     .send(req)
-//     .expect('Content-Type', /json/)
-//     .expect(200).end(function(err, res) {
-//       console.log(res);
-//     });
-//
-//   }
-// }
+  }, function(){});
+  callback();
+}
